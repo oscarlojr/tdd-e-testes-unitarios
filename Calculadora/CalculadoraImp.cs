dbe6972d13843461c9aecd3fd7bbad2bc20ddca3
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2024-05-18 18:04:13 -0300

    feat: Creating the base structure for TDD

--- Calculadora/CalculadoraImp.cs
+++ Calculadora/CalculadoraImp.cs
@@ -7,6 +7,25 @@ namespace Calculadora
 {
     public class CalculadoraImp
     {
-        
+        public int Somar(int num1, int num2)
+        {
+            return 0;
+        }
+        public int Subtrair(int num1, int num2)
+        {
+            return 0;
+        }
+        public int Multiplicar(int num1, int num2)
+        {
+            return 0;
+        }
+        public int Dividir(int num1, int num2)
+        {
+            return 0;
+        }
+        public List<string> Historico()
+        {
+            return new List<string>();
+        }
     }
 }
