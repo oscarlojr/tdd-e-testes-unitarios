b6edb640ecd44fad17437234587a9861bb570261
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2024-05-18 17:57:35 -0300

    feat: Creating a new unit test project

---
+++ CalculadoraTestes/CalculadoraTestes.cs
@@ -0,0 +1,13 @@
+using Calculadora;
+using Xunit;
+
+namespace CalculadoraTestes;
+
+public class CalculadoraTestes
+{
+    [Fact]
+    public void Test1()
+    {
+        CalculadoraImp calc = new CalculadoraImp();
+    }
+}
