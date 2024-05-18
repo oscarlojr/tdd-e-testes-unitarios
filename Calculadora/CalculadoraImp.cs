0fb986b1f4f9c7f661c74f738f615d20507e6d0a
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2024-05-18 18:50:33 -0300

    feat: Challenge TDD and unit test solved

--- Calculadora/CalculadoraImp.cs
+++ Calculadora/CalculadoraImp.cs
@@ -7,25 +7,46 @@ namespace Calculadora
 {
     public class CalculadoraImp
     {
+        private List<string> listaHistorico;
+        private string data;
+
+        public CalculadoraImp(string data)
+        {
+            listaHistorico = new List<string>();
+            this.data = data;
+        }
         public int Somar(int num1, int num2)
         {
-            return 0;
+            int resultado = num1 + num2;
+            listaHistorico.Insert(0, "Resultado: " + resultado + " Data: " + data);
+
+            return resultado;
         }
         public int Subtrair(int num1, int num2)
         {
-            return 0;
+            int resultado = num1 - num2;
+            listaHistorico.Insert(0, "Resultado: " + resultado + " Data: " + data);
+
+            return resultado;
         }
         public int Multiplicar(int num1, int num2)
         {
-            return 0;
+            int resultado = num1 * num2;
+            listaHistorico.Insert(0, "Resultado: " + resultado + " Data: " + data);
+
+            return resultado;
         }
         public int Dividir(int num1, int num2)
         {
-            return 0;
+            int resultado = num1 / num2;
+            listaHistorico.Insert(0, "Resultado: " + resultado + " Data: " + data);
+
+            return resultado;
         }
         public List<string> Historico()
         {
-            return new List<string>();
+            listaHistorico.RemoveRange(3, listaHistorico.Count - 3);
+            return listaHistorico;
         }
     }
 }
